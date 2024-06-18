3b522e7cae7d2ececcdf5bfd2750f2594bbd287b
Diana <m.diana.dev@gmail.com>
2024-06-18 17:50:04 +0300

    fix: fixed slider component

--- src/components/ui/slider/slider.tsx
+++ src/components/ui/slider/slider.tsx
@@ -14,29 +14,26 @@ export const SliderApp = (props: SliderType) => {
   const { max = 99, setValue, step = 1, value } = props
 
   return (
-    <form>
-      <Typography as={'div'} style={{ fontSize: '14px', marginBottom: '5px' }} variant={'body2'}>
-        Number of cards
+    <div className={s.sliderContainer}>
+      <Typography as={'div'} className={s.sliderValue}>
+        {value[0]}
       </Typography>
-      <div className={s.sliderContainer}>
-        <Typography as={'div'} className={s.sliderValue} variant={'body1'}>
-          {value[0]}
-        </Typography>
-        <Slider.Root
-          className={s.SliderRoot}
-          max={max}
-          onValueChange={setValue}
-          step={step}
-          value={value}
-        >
-          <Slider.Track className={s.SliderTrack}>
-            <Slider.Range className={s.SliderRange} />
-          </Slider.Track>
-          <Slider.Thumb aria-label={'Lower value'} className={s.SliderThumb} />
-          <Slider.Thumb aria-label={'Higher value'} className={s.SliderThumb} />
-        </Slider.Root>
-        <div className={s.sliderValue}>{value[1]}</div>
-      </div>
-    </form>
+      <Slider.Root
+        className={s.SliderRoot}
+        max={max}
+        onValueChange={setValue}
+        step={step}
+        value={value}
+      >
+        <Slider.Track className={s.SliderTrack}>
+          <Slider.Range className={s.SliderRange} />
+        </Slider.Track>
+        <Slider.Thumb aria-label={'Lower value'} className={s.SliderThumb} />
+        <Slider.Thumb aria-label={'Higher value'} className={s.SliderThumb} />
+      </Slider.Root>
+      <Typography as={'div'} className={s.sliderValue}>
+        {value[1]}
+      </Typography>
+    </div>
   )
 }
