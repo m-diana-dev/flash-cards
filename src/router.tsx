8e756cdd701d04124861601f35e49c916d63d833
Diana <m.diana.dev@gmail.com>
2024-07-20 12:09:16 +0300

    feat: working with refresh token

--- src/router.tsx
+++ src/router.tsx
@@ -30,7 +30,7 @@ const privateRoutes: RouteObject[] = [
   },
 ]
 
-const router = createBrowserRouter([
+export const router = createBrowserRouter([
   {
     children: [
       {
--- src/services/flashcards-api.ts
+++ src/services/flashcards-api.ts
@@ -1,22 +1,8 @@
-import { createApi, fetchBaseQuery } from '@reduxjs/toolkit/query/react'
+import { baseQueryWithReauth } from '@/services/flashcards-base-query'
+import { createApi } from '@reduxjs/toolkit/query/react'
 
 export const flashcardsApi = createApi({
-  baseQuery: fetchBaseQuery({
-    baseUrl: 'https://api.flashcards.andrii.es',
-    // credentials: 'include',
-    // prepareHeaders: headers => {
-    //   headers.append('x-auth-skip', 'true')
-    // },
-    prepareHeaders: headers => {
-      const token = localStorage.getItem('accessToken')
-
-      if (token) {
-        headers.set('Authorization', `Bearer ${token}`)
-      }
-
-      return headers
-    },
-  }),
+  baseQuery: baseQueryWithReauth,
   endpoints: () => ({}),
   reducerPath: 'flashcardsApi',
   tagTypes: ['Decks', 'Me'],
